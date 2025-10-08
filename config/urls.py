54c64523a63fa9534bbbcff07e54a9ba3d9139c5
Oleh404 <texservise78@gmail.com>
2025-10-08 22:06:32 +0300

    Task 8

--- config/urls.py
+++ config/urls.py
@@ -1,28 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="QA Django HW7 API",
-        default_version="v1",
-        description="Документація до домашки №19",
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("hello.urls")),
-]
-
-# Swagger + ReDoc
-urlpatterns += [
-    re_path(r"^swagger(?P<format>\.json|\.yaml)$",
-            schema_view.without_ui(cache_timeout=0), name="schema-json"),
-    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("hello/", include("hello.urls")),  # наш app
 ]
